620143e5078ccf03d58d5f37dd7714f35b49178b
Hambirth <102298849+Hambirth@users.noreply.github.com>
2023-09-07 12:31:23 +0530

    Countdowncompleted

---
+++ Js/app.js
@@ -0,0 +1,32 @@
+const endDate = "27 JUNE 2023 11:00 PM "
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+
+    const diff = (end - now) / 1000;
+
+    //convert into milisec
+
+
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600 )% 24;
+    inputs[2].value = Math.floor(diff / 60 )% 60;
+    inputs[3].value = Math.floor(diff  )% 60;
+}
+
+
+
+
+clock()
+
+
+
+setInterval(
+    ()=>{
+        clock()
+    }, 
+    1000
+)
